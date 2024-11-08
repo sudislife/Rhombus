fa600f7acf0583c4e57857ff4aebcee9c716e644
Cheeky <sudaksh77@gmail.com>
2024-11-08 14:34:46 +1100

    Created a filr upload function page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,6 +1,6 @@
 import { title, subtitle } from "@/components/primitives";
 
-export default function FileUploadPage() {
+export default function Home() {
     return (
         <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
             <div className="inline-block max-w-xl text-center justify-center">
