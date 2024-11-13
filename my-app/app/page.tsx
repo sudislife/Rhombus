1c8ba9ea4ad08bc45adec6bad5cb19aa8ba26254
Cheeky <sudaksh77@gmail.com>
2024-11-13 13:50:42 +1100

    Experimenting different file upload pages

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,12 +1,14 @@
-import { title, subtitle } from "@/components/primitives";
+import { title } from "@/components/primitives";
+import FileUpload from "./file_upload/page";
 
 export default function Home() {
-    return (
-        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-            <div className="inline-block max-w-xl text-center justify-center">
-                <span className={title()}>Upload your file here&nbsp;</span>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-xl text-center justify-center">
+        <span className={title()}>Dynamic Data Inference&nbsp;</span>
+      </div>
 
+      <FileUpload />
+    </section>
+  );
+}
